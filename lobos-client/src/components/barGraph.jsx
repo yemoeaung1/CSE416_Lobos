5a93068b86c084d6739c7443b4d1828f1790d9d4
Cody Lam <143743411+codlam26@users.noreply.github.com>
2024-10-01 21:02:20 -0400

    Working on the graphs

--- lobos-client/src/components/barGraph.jsx
+++ lobos-client/src/components/barGraph.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import Chart from 'chart.js/auto';
 import { Minus, X, Minimize, Maximize} from 'lucide-react';
+import { Rnd } from "react-rnd"
 
 const BarGraph = () =>{
     const chartRef = useRef(null)
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    const initialX = (screenWidth - 384) / 2;
+    const initialY = (screenHeight - 384) / 2;
 
     useEffect(() => {
         const ctx = chartRef.current.getContext("2d");
@@ -36,24 +43,45 @@ const BarGraph = () =>{
         }
     }, [])
 
+
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="w-96 h-96 border-2 border-gray-700 rounded-xl p-8">
-                <div>
+        <div className="min-h-screen">
+          <Rnd
+          default={{
+            x: initialX,
+            y: initialY,
+            width: 384,
+            height: 384,
+          }}>
+            <div className="w-full h-full border-2 border-gray-600 rounded-xl shadow-2xl">
+                
+                <div className="p-4 flex justify-end space-x-2">
                     <button className="text-gray-500 pr-2 hover:text-gray-700">
-                        <X size={16}/>
+                        <Minus size={16}/>
                     </button>
+                    
                     <button className="text-gray-500 pr-2 hover:text-gray-700">
                         <Maximize size={16}/>
                     </button>
+
                     <button className="text-gray-500 hover:text-gray-700">
-                        <Minus size={16}/>
+                        <X size={16}/>
                     </button>
-                    <canvas ref={chartRef} width={600} height={600}></canvas>
                 </div>
 
+                <div className="flex justify-center mb-2">
+                  <h1 className="font-bold text-2xl">
+                    Title of the Chart
+                  </h1>
+                </div>
+
+                <div className="">
+                  <canvas className="pl-4 pr-4" ref={chartRef} width={500} height={400}></canvas>
+                </div>
             </div>
+          </Rnd>
         </div>
+      
         
     )
 }
