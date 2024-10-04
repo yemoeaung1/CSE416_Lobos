57750e33264beff3aa888196b5f6a23ebf494fd9
Kelvin Lin <kelvin.lin@stonybrook.edu>
2024-10-03 21:54:00 -0400

    Changed colors on hover and select, added new element to Data Toolbar

--- lobos-client/src/components/DataContainer.jsx
+++ lobos-client/src/components/DataContainer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { BsArrowBarLeft, BsArrowBarRight, BsInfoCircle, BsFillBarChartFill, BsBarChart } from "react-icons/bs";
+import { BsArrowBarLeft, BsArrowBarRight, BsInfoCircle, BsFillBarChartFill, BsGraphUp } from "react-icons/bs";
 import BarGraph from "./barGraph";
 
 function DataContainer({isOpen, setIsOpen, selectedArea}) {
@@ -16,7 +16,7 @@ function DataContainer({isOpen, setIsOpen, selectedArea}) {
       }
 
       <div>
-        <DataToolbar isOpen={isOpen} setDataTool={setDataTool} />
+        <DataToolbar isOpen={isOpen} dataTool={dataTool} setDataTool={setDataTool} />
         <DataComponent isOpen={isOpen} dataTool={dataTool}/>
       </div>
 
@@ -24,19 +24,24 @@ function DataContainer({isOpen, setIsOpen, selectedArea}) {
   );
 }
 
-function DataToolbar({ isOpen, setDataTool}){
+function DataToolbar({ isOpen, dataTool, setDataTool}){
 
     return (
       <div className={`data-toolbar ${isOpen ? "open" : ""}`}>
 
-        <div className="toolbar-item">
-          <BsInfoCircle className="toolbar-icon" onClick = {() => setDataTool('info')}/>
-          <span className="toolbar-label">Info</span>
+        <div className={`toolbar-item ${dataTool === 'info' ? "tool-selected" : ""}`} onClick = {() => setDataTool('info')}>
+          <BsInfoCircle className="toolbar-icon"/>
+          <span className="toolbar-label">State Info</span>
         </div>
 
-        <div className="toolbar-item">
-          <BsFillBarChartFill className="toolbar-icon" onClick = {() => setDataTool('graph')}/>
+        <div className={`toolbar-item ${dataTool === 'graph' ? "tool-selected" : ""}`} onClick = {() => setDataTool('graph')}>
+          <BsFillBarChartFill className="toolbar-icon"/>
           <span className="toolbar-label">Graphs</span>
+        </div>
+
+        <div className={`toolbar-item ${dataTool === 'analysis' ? "tool-selected" : ""}`} onClick = {() => setDataTool('analysis')}>
+          <BsGraphUp className="toolbar-icon"/>
+          <span className="toolbar-label">Analysis</span>
         </div>
         
       </div>
