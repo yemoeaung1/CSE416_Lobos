f4e658d4a491c378249fffc1215b6c9a2ee116b1
Cody Lam <143743411+codlam26@users.noreply.github.com>
2024-10-03 22:21:07 -0400

    Completed the Graph Page

--- lobos-client/src/components/DataContainer.jsx
+++ lobos-client/src/components/DataContainer.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 
 import { BsArrowBarLeft, BsArrowBarRight, BsInfoCircle, BsFillBarChartFill, BsBarChart } from "react-icons/bs";
 import BarGraph from "./barGraph";
+import LineGraph from "./lineGraph";
+import BoxPlotGraph from "./boxPlotGraph";
+
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
 
 function DataContainer({isOpen, setIsOpen, selectedArea}) {
   const [dataTool, setDataTool ] = useState('info')
@@ -44,6 +50,7 @@ function DataToolbar({ isOpen, setDataTool}){
 }
 
 function DataComponent({ isOpen, dataTool}){
+
   return (
     <div className={`data-component ${isOpen ? "open" : ""}`}>
       
@@ -52,18 +59,57 @@ function DataComponent({ isOpen, dataTool}){
           <h1>
             State Info:
           </h1>
+          
         </div> 
       )}
       
       {dataTool === 'graph' && (
-        <div className = 'text-black' >
-          <BarGraph/>
-        </div>
+          <DataComponent_Graph/>
       )}
-      
 
     </div>
   );
+}
+
+function DataComponent_Graph(){
+  const [graphType, setGraphType] = useState('bar')
+
+  const handleSwitchChange = (event, type) => {
+    if (event.target.checked) {
+      setGraphType(type);
+    }
+  };
+
+    return(
+      <div className="flex flex-col h-full">
+          <div className = "mb-8">
+              <FormControlLabel control={<Switch defaultChecked checked={graphType === "bar"} 
+              onChange={(e) => handleSwitchChange(e, "bar")} />} label="Bar Graph" />
+
+              <FormControlLabel control={<Switch checked={graphType === "box"} 
+              onChange={(e) => handleSwitchChange(e, "box")} />} label="Box Plot" />
+
+              <FormControlLabel control={<Switch  checked={graphType === "line"} 
+              onChange={(e) => handleSwitchChange(e, "line")}/>} label="Line Graph" />
+          </div>
+          
+          <div className="h-3/4 w-full">
+            
+            {graphType === 'bar' && (
+                <BarGraph/>
+            )}
+
+            {graphType === 'box' && (
+                <BoxPlotGraph/>
+            )}
+
+            {graphType === 'line' && (
+                <LineGraph/>
+            )}
+
+          </div>
+        </div>
+    )
 }
 
 export default DataContainer;
