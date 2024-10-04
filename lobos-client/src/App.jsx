7cf95d83bff1389e1a4831082278e1e72819661f
Kelvin Lin <kelvin.lin@stonybrook.edu>
2024-10-03 20:15:05 -0400

    Fixed App.jsx to work with DataContainer

--- lobos-client/src/App.jsx
+++ lobos-client/src/App.jsx
@@ -3,19 +3,30 @@ import "./App.css";
 import StatesMap from "./components/StatesMap";
 import NavBar from "./components/NavBar";
 import MapLayerSelector from "./components/MapLayerSelector";
+import DataContainer from "./components/DataContainer"
 
 function App() {
   const [selectedArea, setSelectedArea] = useState('none');
   const [mapView, setMapView] = useState('State');
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if(selectedArea !== 'none' && !isOpen)
+      setIsOpen(true);
+    else if(selectedArea === 'none' && isOpen)
+      setIsOpen(false);
+  }, [selectedArea]);
 
   console.log(selectedArea);
   console.log(mapView);
 
   return (
     <div>
-      <NavBar setMapView={setMapView} setSelectedArea={setSelectedArea} />
+      <NavBar selectedArea={selectedArea} setMapView={setMapView} setSelectedArea={setSelectedArea} />
+      <DataContainer isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div className="wrapper" style={{
-        width: selectedArea !== 'none' ? '40%' : '100%'
+        width: selectedArea !== 'none' ? '38%' : '98%'
       }}>
         <StatesMap
           setSelectedArea={setSelectedArea}
