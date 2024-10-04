ca47226d7251547a5356e2b3c4931fe0b48272df
Cody Lam <143743411+codlam26@users.noreply.github.com>
2024-10-04 02:54:04 -0400

    Finished the Basic UI for the graphs page in the data container

--- lobos-client/src/components/DataContainer.jsx
+++ lobos-client/src/components/DataContainer.jsx
@@ -5,9 +5,12 @@ import BarGraph from "./barGraph";
 import LineGraph from "./lineGraph";
 import BoxPlotGraph from "./boxPlotGraph";
 
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import IncomeVotingScatter from "./IncomeVotingScatter";
 
 function DataContainer({isOpen, setIsOpen, selectedArea}) {
   const [dataTool, setDataTool ] = useState('info')
@@ -37,7 +40,7 @@ function DataToolbar({ isOpen, dataTool, setDataTool}){
 
         <div className={`toolbar-item ${dataTool === 'info' ? "tool-selected" : ""}`} onClick = {() => setDataTool('info')}>
           <BsInfoCircle className="toolbar-icon"/>
-          <span className="toolbar-label">State Info</span>
+          <span className="toolbar-label">Info</span>
         </div>
 
         <div className={`toolbar-item ${dataTool === 'graph' ? "tool-selected" : ""}`} onClick = {() => setDataTool('graph')}>
@@ -70,6 +73,12 @@ function DataComponent({ isOpen, dataTool}){
       
       {dataTool === 'graph' && (
           <DataComponent_Graph/>
+      )}  
+
+      {dataTool === 'analysis' && (
+        <div>
+          <IncomeVotingScatter/>
+        </div>
       )}
 
     </div>
@@ -78,40 +87,72 @@ function DataComponent({ isOpen, dataTool}){
 
 function DataComponent_Graph(){
   const [graphType, setGraphType] = useState('bar')
+  const [dataSetType, setDataSetType] = useState('party');
 
   const handleSwitchChange = (event, type) => {
     if (event.target.checked) {
       setGraphType(type);
+
+    if (type !== "bar" && dataSetType === "party") {
+      setDataSetType("race");
+    }
     }
   };
 
     return(
       <div className="flex flex-col h-full">
-          <div className = "mb-8">
-              <FormControlLabel control={<Switch defaultChecked checked={graphType === "bar"} 
-              onChange={(e) => handleSwitchChange(e, "bar")} />} label="Bar Graph" />
+          <div className = "mb-8 flex justify-between ">
 
-              <FormControlLabel control={<Switch checked={graphType === "box"} 
-              onChange={(e) => handleSwitchChange(e, "box")} />} label="Box Plot" />
+            <FormControl>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={{ fontSize: '1.5rem', fontWeight: 'bold' }}>Graph Filters: </FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                 
+                  <FormControlLabel value="bar " control={<Radio checked={graphType === "bar"} 
+                  onChange={(e) => handleSwitchChange(e, "bar")} />} label="Bar Graph" />
 
-              <FormControlLabel control={<Switch  checked={graphType === "line"} 
-              onChange={(e) => handleSwitchChange(e, "line")}/>} label="Line Graph" />
+                  {dataSetType !== "party" && (
+                  <>
+                    <FormControlLabel value="box" control={<Radio checked={graphType === "box"}  
+                    onChange={(e) => handleSwitchChange(e, "box")}/>} label="Box Plot" />
+                    <FormControlLabel value="line" control={<Radio checked={graphType === "line"} 
+                    onChange={(e) => handleSwitchChange(e, "line")}/>} label="Line Graph" />
+                  </>
+                  )}
+                  
+                </RadioGroup>
+            </FormControl>
+
+            {/*Buttons on the top right corner*/}
+            <div className="mt-5">
+              {graphType === 'bar' && 
+                <button className= {dataSetType === 'party' ? 'text-2xl font-semibold border-2 border-black rounded-xl mr-4 p-1 pl-4 pr-4 bg-blue-400 shadow-2xl text-white' :
+                'text-2xl font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 hover:bg-blue-200 shadow-2xl'}
+                onClick={() => {setDataSetType("party")}}>Party </button>
+              }
+            
+              <button className= {dataSetType === 'race' ? 'text-2xl font-semibold border-2 border-black rounded-xl mr-4 p-1 pl-4 pr-4 bg-blue-400 shadow-2xl text-white' :
+              'text-2xl font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 hover:bg-blue-200 shadow-2xl'}
+              onClick={() => setDataSetType("race")}>Race </button>
+
+              <button className={dataSetType === 'income' ? 'text-2xl font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 bg-blue-400 shadow-2xl text-white' :
+              'text-2xl font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 hover:bg-blue-200 shadow-2xl'}
+              onClick={() => setDataSetType("income")}> Income </button>
+
+              <button className={dataSetType === 'age' ? 'text-2xl font-semibold border-2 border-black rounded-xl p-1 pl-4 pr-4 bg-blue-400 shadow-2xl text-white' :
+              'text-2xl font-semibold border-2 border-black rounded-xl p-1 pl-4 pr-4 hover:bg-blue-200 shadow-2xl'}
+              onClick={() => setDataSetType("age")}> Age </button>
+          </div>
+
           </div>
           
           <div className="h-3/4 w-full">
-            
-            {graphType === 'bar' && (
-                <BarGraph/>
-            )}
-
-            {graphType === 'box' && (
-                <BoxPlotGraph/>
-            )}
-
-            {graphType === 'line' && (
-                <LineGraph/>
-            )}
-
+            {graphType === "bar" && <BarGraph dataSetType={dataSetType}/>}
+            {graphType === "box" && <BoxPlotGraph dataSetType={dataSetType}/>}
+            {graphType === "line" && <LineGraph dataSetType={dataSetType}/>}
           </div>
         </div>
     )
--- lobos-client/src/components/barGraph.jsx
+++ lobos-client/src/components/barGraph.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Chart from 'chart.js/auto';
 
 
-const BarGraph = () =>{
+const BarGraph = ({ dataSetType }) =>{
     const chartRef = useRef(null);
     const [chartInstance, setChartInstance] = useState(null);
     const [currentDataset, setCurrentDataset] = useState("race")
@@ -10,36 +10,80 @@ const BarGraph = () =>{
     useEffect(() => {
         const ctx = chartRef.current.getContext("2d");
 
-        const data = [3000, 4943, 5920, 4134, 4234];
-        const labels = ["Asian", "Hispanic", "Black", "White", "American Indian"]
+        const dataSets = {
+          party: {
+            labels: ["Parties"],
+            data: [
+              { label: "Republican", data: [8020], backgroundColor: "rgba(255, 0, 0, 0.5)" },
+              { label: "Democrat", data: [1890], backgroundColor: "rgba(0, 0, 255, 0.5)" },
+              { label: "Independent", data: [300], backgroundColor: "rgba(0, 255, 0, 0.5)" }
+            ],
+            title: "Support for Candidates by Party",
+            xTitle: "Political Party"
+          },
+          race: {
+            labels: ["Asian", "Hispanic", "Black", "White", "American Indian"],
+            data: [
+              { label: "Republican", data: [3000, 4943, 5920, 4134, 4234], backgroundColor: "rgba(255, 0, 0, 0.5)" },
+              { label: "Democrat", data: [3000, 894, 590, 4234, 4234], backgroundColor: "rgba(0, 0, 255, 0.5)" },
+              { label: "Independent", data: [30, 89, 59, 42, 44], backgroundColor: "rgba(0, 255, 0, 0.5)" }
+            ],
+            title: "Support for Candidates by Racial Group",
+            xTitle: "Ethnicity"
+          },
+          income: {
+            labels: ["<25k", "25-50k", "50-75k", "75-100k", ">100k"],
+            data: [
+              { label: "Republican", data: [2000, 1500, 3000, 4000, 6000], backgroundColor: "rgba(255, 0, 0, 0.5)" },
+              { label: "Democrat", data: [1800, 1200, 2800, 3500, 5800], backgroundColor: "rgba(0, 0, 255, 0.5)" },
+              { label: "Independent", data: [100, 90, 200, 300, 400], backgroundColor: "rgba(0, 255, 0, 0.5)" }
+            ],
+            title: "Support for Candidates by Income Bracket",
+            xTitle: "Income"
+          },
+          age: {
+            labels: ["18-25", "26-30", "36-42", "43-49", "65+"],
+            data: [
+              { label: "Republican", data: [2300, 1400, 1000, 2000, 4000], backgroundColor: "rgba(255, 0, 0, 0.5)" },
+              { label: "Democrat", data: [4300, 3200, 1800, 2500, 4800], backgroundColor: "rgba(0, 0, 255, 0.5)" },
+              { label: "Independent", data: [60, 40, 20, 360, 99], backgroundColor: "rgba(0, 255, 0, 0.5)" }
+            ],
+            title: "Support for Candidates by Age Group",
+            xTitle: "Age"
+          }
+        };
+    
+        const { labels, data, title, xTitle} = dataSets[dataSetType];
 
         const BarChart = new Chart(ctx, {
             type: "bar",
             data: {
-                labels: ["Asian", "Hispanic", "Black", "White", "American Indian"],
-                datasets: [
-                  {
-                    label: "Republican",
-                    data: [3000, 4943, 5920, 4134, 4234], 
-                    backgroundColor: "rgba(255, 0, 0, 0.5)",
-                    borderColor: "rgba(255, 0, 0, 1)",
-                    borderWidth: 1
-                  },
-                  {
-                    label: "Democrat",
-                    data: [3000, 894, 590, 4234, 4234], 
-                    backgroundColor: "rgba(0, 0, 255, 0.5)",
-                    borderColor: "rgba(0, 0, 255, 1)",
-                    borderWidth: 1
-                  },
-                  {
-                    label: "Independent",
-                    data: [30, 89, 59, 42, 44], 
-                    backgroundColor: "rgba(0, 255, 0, 0.5)",
-                    borderColor: "rgba(0, 255, 0, 1)",
-                    borderWidth: 1
-                  },
-                ],
+                // labels: ["Asian", "Hispanic", "Black", "White", "American Indian"],
+                labels: labels,
+                // datasets: [
+                //   {
+                //     label: "Republican",
+                //     data: [3000, 4943, 5920, 4134, 4234], 
+                //     backgroundColor: "rgba(255, 0, 0, 0.5)",
+                //     borderColor: "rgba(255, 0, 0, 1)",
+                //     borderWidth: 1
+                //   },
+                //   {
+                //     label: "Democrat",
+                //     data: [3000, 894, 590, 4234, 4234], 
+                //     backgroundColor: "rgba(0, 0, 255, 0.5)",
+                //     borderColor: "rgba(0, 0, 255, 1)",
+                //     borderWidth: 1
+                //   },
+                //   {
+                //     label: "Independent",
+                //     data: [30, 89, 59, 42, 44], 
+                //     backgroundColor: "rgba(0, 255, 0, 0.5)",
+                //     borderColor: "rgba(0, 255, 0, 1)",
+                //     borderWidth: 1
+                //   },
+                // ],
+                datasets: data
               },
               options: {
                 responsive: true,
@@ -47,11 +91,16 @@ const BarGraph = () =>{
                   x:{
                     title:{
                       display: true,
-                      text: "Ethnicity",
+                      text: xTitle,
                       font: {
-                        size: 15,
+                        size: 20,
                       },
                       color: '#000000'
+                    },
+                    ticks:{
+                      font:{
+                        size: 18
+                      }
                     }
                   },
                   y: {
@@ -60,22 +109,34 @@ const BarGraph = () =>{
                       display: true,
                       text: 'Number of Voters',
                       font: {
-                        size: 15,
+                        size: 20,
                       },
                       color: '#000000'
+                    },
+                    ticks:{
+                      font:{
+                        size: 18
+                      }
                     }
                   },
                 },
                 plugins:{
                   legend: {
                     position: 'top',
+                    labels:{
+                      font:{
+                        size:16
+                      }
+                    }
                   },
                   title: {
                     display: true,
-                    text: 'Support for Candidates by Racial Group',
+                    text: title,
                     font: {
-                      size: 20,
-                    }
+                      size: 28,
+                      weight: 'bold',
+                    },
+                  color: '#000000'
 
                   }
                 }
@@ -87,58 +148,44 @@ const BarGraph = () =>{
         return () => {
             BarChart.destroy()
         }
-    }, [])
+    }, [dataSetType])
 
-    const updateChart = (type) => {
-      if (!chartInstance) return;
+    // const updateChart = (type) => {
+    //   if (!chartInstance) return;
   
-      if (type === "race") {
-        setCurrentDataset('race')
-        chartInstance.data.labels = ["Asian", "Hispanic", "Black", "White", "American Indian"];
-        chartInstance.data.datasets[0].data = [3000, 4943, 5920, 4134, 4234];
-        chartInstance.data.datasets[1].data = [3000, 894, 590, 4234, 4234];
-        chartInstance.data.datasets[2].data = [30, 89, 59, 42, 44];
-        chartInstance.options.plugins.title.text = "Support for Candidates by Racial Group";
-        chartInstance.options.scales.x.title.text = "Ethnicity"
-      } 
-      else if (type === "income") {
-        setCurrentDataset('income')
-        chartInstance.data.labels = ["<25k", "25-50k", "50-75k", "75-100k", ">100k"];
-        chartInstance.data.datasets[0].data = [2000, 1500, 3000, 4000, 6000];
-        chartInstance.data.datasets[1].data = [1800, 1200, 2800, 3500, 5800];
-        chartInstance.data.datasets[2].data = [100, 90, 200, 300, 400];
-        chartInstance.options.plugins.title.text = "Support for Candidates by Income";
-        chartInstance.options.scales.x.title.text = "Income"
-      }
-      else if (type === "age"){
-        setCurrentDataset('age')
-        chartInstance.data.labels = ["18-25", "26-30", "36-42", "43-49", "65+"];
-        chartInstance.data.datasets[0].data = [2300, 1400, 1000, 2000, 4000];
-        chartInstance.data.datasets[1].data = [4300, 3200, 1800, 2500, 4800];
-        chartInstance.data.datasets[2].data = [60, 40, 20, 360, 99];
-        chartInstance.options.plugins.title.text = "Support for Candidates by Age";
-        chartInstance.options.scales.x.title.text = "Age"
-      }
+    //   if (type === "race") {
+    //     setCurrentDataset('race')
+    //     chartInstance.data.labels = ["Asian", "Hispanic", "Black", "White", "American Indian"];
+    //     chartInstance.data.datasets[0].data = [3000, 4943, 5920, 4134, 4234];
+    //     chartInstance.data.datasets[1].data = [3000, 894, 590, 4234, 4234];
+    //     chartInstance.data.datasets[2].data = [30, 89, 59, 42, 44];
+    //     chartInstance.options.plugins.title.text = "Support for Candidates by Racial Group";
+    //     chartInstance.options.scales.x.title.text = "Ethnicity"
+    //   } 
+    //   else if (type === "income") {
+    //     setCurrentDataset('income')
+    //     chartInstance.data.labels = ["<25k", "25-50k", "50-75k", "75-100k", ">100k"];
+    //     chartInstance.data.datasets[0].data = [2000, 1500, 3000, 4000, 6000];
+    //     chartInstance.data.datasets[1].data = [1800, 1200, 2800, 3500, 5800];
+    //     chartInstance.data.datasets[2].data = [100, 90, 200, 300, 400];
+    //     chartInstance.options.plugins.title.text = "Support for Candidates by Income";
+    //     chartInstance.options.scales.x.title.text = "Income"
+    //   }
+    //   else if (type === "age"){
+    //     setCurrentDataset('age')
+    //     chartInstance.data.labels = ["18-25", "26-30", "36-42", "43-49", "65+"];
+    //     chartInstance.data.datasets[0].data = [2300, 1400, 1000, 2000, 4000];
+    //     chartInstance.data.datasets[1].data = [4300, 3200, 1800, 2500, 4800];
+    //     chartInstance.data.datasets[2].data = [60, 40, 20, 360, 99];
+    //     chartInstance.options.plugins.title.text = "Support for Candidates by Age";
+    //     chartInstance.options.scales.x.title.text = "Age"
+    //   }
   
-      chartInstance.update();
-    };
+    //   chartInstance.update();
+    // };
 
     return (
         <div className= " border-2 border-gray-800 rounded-xl shadow-xl">
-          <div className="border-gray-800 justify-center flex m-8">
-            <button className= {currentDataset === 'race' ? 'border-2 border-black rounded-xl mr-4 p-1 pl-4 pr-4 bg-red-400' :
-            'border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 hover:bg-red-200'}
-            onClick={() => updateChart("race")}>Race </button>
-
-            <button className={currentDataset === 'income' ? 'border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 bg-red-400' :
-            'border-2 border-black mr-4 rounded-xl p-1 pl-4 pr-4 hover:bg-red-200'}
-             onClick={() => updateChart("income")}> Income </button>
-
-             <button className={currentDataset === 'age' ? 'border-2 border-black rounded-xl p-1 pl-4 pr-4 bg-red-400' :
-            'border-2 border-black rounded-xl p-1 pl-4 pr-4 hover:bg-red-200'}
-             onClick={() => updateChart("age")}> Age </button>
-          </div>
-
           <div className="flex-1 flex justify-center items-center">
             <canvas ref={chartRef} ></canvas>
           </div>
