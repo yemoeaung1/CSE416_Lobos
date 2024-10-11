e60fbad1bfa0cd6ac628848892b0afa2efc36124
Kelvin Lin <kelvin.lin@stonybrook.edu>
2024-10-11 03:18:04 -0400

    More design for State Info DataComponent and removed a console log

--- lobos-client/src/components/DataContainer.jsx
+++ lobos-client/src/components/DataContainer.jsx
@@ -155,12 +155,12 @@ function DataComponent_Info({ selectedArea, selectedState, dataMapping }) {
         <div className="data-component-info-top-left">
           <div className="data-component-info-stat-box">
             <span className="font-bold underline merriweather pb-2">Population</span>
-            <span className="lato">{`${population.toLocaleString()}`}</span>
+            <span className="montserrat">{`${population.toLocaleString()}`}</span>
           </div>
           <div className="data-component-info-stat-box">
             <span className="font-bold underline merriweather">Median</span>
             <span className="font-bold underline merriweather pb-2">Household Income</span>
-            <span className="lato">{`${income.toLocaleString()}`}</span>
+            <span className="montserrat">{`${income.toLocaleString()}`}</span>
           </div>
         </div>
         <div className="data-component-info-top-middle p-1">
@@ -174,17 +174,18 @@ function DataComponent_Info({ selectedArea, selectedState, dataMapping }) {
         <div className="data-component-info-top-right">
           <div className="data-component-info-stat-box">
             <span className="font-bold underline merriweather pb-2">Majority Race</span>
-            <span className="lato">{`${race}`}</span>
+            <span className="montserrat">{`${race}`}</span>
           </div>
           <div className="data-component-info-stat-box">
             <span className="font-bold underline merriweather pb-2">Majority Party</span>
-            <span className="lato">{`${party}`}</span>
+            <span className="montserrat">{`${party}`}</span>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <span className="font-bold underline merriweather text-4xl mb-16">Precinct/District/County Data</span>
-        <span className="font-bold underline lato text-2xl">{(selectedArea === selectedState) ? "[Select A Precinct/District/County]" : "[Information Goes Here]"}</span>
+            <RepresentativesData dataMapping={dataMapping} selectedState={selectedState}/>
+        {/* <span className="font-bold underline merriweather text-4xl mb-16">Precinct/District/County Data</span>
+        <span className="font-bold underline lato text-2xl">{(selectedArea === selectedState) ? "[Select A Precinct/District/County]" : "[Information Goes Here]"}</span> */}
       </div>
     </>
   );
@@ -370,5 +371,39 @@ function DataComponent_Gingles() {
     </div>
   );
 }
+
+
+function RepresentativesData ({ dataMapping, selectedState }) {
+  let districts = (dataMapping && selectedState in dataMapping) ? dataMapping[selectedState]["Congressional Districts"] : null;
+
+  if(districts === null) {
+    return <></>;
+  } else {
+    districts = Object.entries(districts);
+    return (
+      <div className="table-container">
+          <table className="merriweather congress-table">
+        <thead>
+          <tr>
+            <th className="text-xl">Congressional District</th>
+            <th className="text-xl">Representative</th>
+            <th className="text-xl">Party</th>
+          </tr>
+        </thead>
+        <tbody>
+          {districts.map(([districtName, details]) => (
+            <tr key={districtName} className={details.Party === 'Democratic' ? 'hover:text-blue-500' : 'hover:text-red-500'}>
+              <td>{districtName}</td>
+              <td>{details.Representative}</td>
+              <td>{details.Party}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+
+  );
+  }
+};
 
 export default DataContainer;
