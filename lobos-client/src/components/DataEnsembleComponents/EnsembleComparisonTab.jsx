e8847b44f3cc7fe161138d73b8722eabf86b1326
Kelvin Lin <kelvin.lin@stonybrook.edu>
2024-12-17 04:23:38 -0500

    Fixed styling of Ensemble Comparison Tab so that map is always visible

--- lobos-client/src/components/DataEnsembleComponents/EnsembleComparisonTab.jsx
+++ lobos-client/src/components/DataEnsembleComponents/EnsembleComparisonTab.jsx
@@ -2,8 +2,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { MapContainer, GeoJSON } from "react-leaflet";
 import Color from "color";
-import { Box, Button, IconButton, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
+import { IconButton, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
 import { DataFilters, States } from "../../enums";
 import BarGraph from "../GraphPlotComponents/BarGraph";
 
@@ -16,8 +17,6 @@ export default function EnsembleComparisonTab({ selectedState }) {
     const [planGeoJSON, setPlanGeoJSON] = useState(null);
     const [planGraphData, setPlanGraphData] = useState(null);
     const [planPopulationData, setPlanPopulationData] = useState(0);
-
-    const [showPlan, setShowPlan] = useState(false);
 
     useEffect(() => {
         if (selectedState !== States.NONE && selectedPlan !== 'None') {
@@ -70,7 +69,7 @@ export default function EnsembleComparisonTab({ selectedState }) {
                     console.error("Error Retrieving District Plan Data:", error);
                 });
         }
-    }, [selectedPlan, selectedDist]);
+    }, [selectedPlan, selectedDist, selectedDistrict]);
 
     const resetStates = () => {
         setSelectedDist(DataFilters.PARTY);
@@ -87,10 +86,57 @@ export default function EnsembleComparisonTab({ selectedState }) {
                 selectedState={selectedState}
                 selectedPlan={selectedPlan}
                 setSelectedPlan={setSelectedPlan}
-                setShowPlan={setShowPlan}
                 resetStates={resetStates}
             />
             <div className="mt-4 mb-4" />
+            <DistrictPlanContent
+                planGraphData={planGraphData}
+                planPopulationData={planPopulationData}
+                selectedDistrict={selectedDistrict}
+                selectedDist={selectedDist}
+                setSelectedDist={setSelectedDist}
+                planGeoJSON={planGeoJSON}
+                setSelectedDistrict={setSelectedDistrict}
+            />
+        </>
+    );
+}
+
+function DistrictPlanSelection({ selectedState, selectedPlan, setSelectedPlan, resetStates }) {
+    const districtPlans = availablePlans[selectedState];
+
+    return (
+        <FormControl fullWidth sx={{ mr: 1 }}>
+            <InputLabel id="district-plan-dropdown-label" sx={{ fontFamily: "Montserrat, san-serif" }}>District Plans</InputLabel>
+            <Select
+                labelId="district-plan-dropdown-label"
+                value={selectedPlan}
+                onChange={(event) => {
+                    setSelectedPlan(event.target.value);
+                    resetStates();
+                }}
+                label="District Plans"
+                sx={{
+                    fontFamily: "Montserrat, san-serif",
+                }}
+            >
+                {districtPlans.map((option, index) => (
+                    <MenuItem key={index} value={option} sx={{ fontFamily: "Montserrat, san-serif" }}>
+                        {option}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+}
+
+function DistrictPlanContent({ planGraphData, planPopulationData, selectedDistrict, selectedDist, setSelectedDist, planGeoJSON, setSelectedDistrict }) {
+    return (
+        <div className="flex flex-row">
+            <DistrictPlanPopup
+                planGeoJSON={planGeoJSON}
+                setSelectedDistrict={setSelectedDistrict}
+            />
             <DistrictPlanSummary
                 planGraphData={planGraphData}
                 planPopulationData={planPopulationData}
@@ -98,58 +144,72 @@ export default function EnsembleComparisonTab({ selectedState }) {
                 selectedDist={selectedDist}
                 setSelectedDist={setSelectedDist}
             />
-            {showPlan && <DistrictPlanPopup planGeoJSON={planGeoJSON} setShowPlan={setShowPlan} />}
-        </>
+        </div>
+    );
+}
+function DistrictPlanPopup({ planGeoJSON, setSelectedDistrict }) {
+    if (!planGeoJSON)
+        return;
+
+    return (
+        <div className="district-plan-popup">
+            <IconButton className="district-plan-popup-exit" size="small" component="span" onClick={() => {  }}>
+                <FullscreenIcon />
+            </IconButton>
+            <DistrictPlanMap planGeoJSON={planGeoJSON} setSelectedDistrict={setSelectedDistrict} />
+        </div>
     );
 }
 
-function DistrictPlanSelection({ selectedState, selectedPlan, setSelectedPlan, setShowPlan, resetStates }) {
-    const districtPlans = availablePlans[selectedState];
+function DistrictPlanMap({ planGeoJSON, setSelectedDistrict }) {
+    if (planGeoJSON === null || planGeoJSON.geoJSON === null || planGeoJSON.properties === null)
+        return;
 
-    const isDisabled = (selectedPlan === 'None');
+    const onEachFeature = (feature, layer) => {
+        let originalColor = "#FFFFFF";
+
+        const darkerColor = Color(originalColor).darken(0.25).hex();
+
+        layer.setStyle({
+            fillColor: originalColor,
+            fillOpacity: feature.properties.FOPACITY || 0.75,
+            color: feature.properties.COLOR || "#000000",
+            weight: 1,
+        });
+
+        layer.on({
+            mouseover: (e) => {
+                e.target.setStyle({ fillColor: darkerColor, weight: 3 });
+            },
+            mouseout: (e) => {
+                e.target.setStyle({ fillColor: originalColor, weight: 1 });
+            },
+            click: (e) => {
+                setSelectedDistrict(feature.properties.NAME);
+            }
+        });
+    };
 
     return (
-        <Box display="flex" alignItems="center">
-            <FormControl fullWidth sx={{ mr: 1 }}>
-                <InputLabel id="district-plan-dropdown-label" sx={{ fontFamily: "Montserrat, san-serif" }}>District Plans</InputLabel>
-                <Select
-                    labelId="district-plan-dropdown-label"
-                    value={selectedPlan}
-                    onChange={(event) => {
-                        setSelectedPlan(event.target.value);
-                        setShowPlan(false);
-                        resetStates();
-                    }}
-                    label="District Plans"
-                    sx={{
-                        fontFamily: "Montserrat, san-serif",
-                    }}
-                >
-                    {districtPlans.map((option, index) => (
-                        <MenuItem key={index} value={option} sx={{ fontFamily: "Montserrat, san-serif" }}>
-                            {option}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <div className="pr-2 pl-6">
-                <Button
-                    onClick={() => setShowPlan(true)}
-                    sx={{
-                        textTransform: 'none',
-                        padding: "2px 4px",
-                        fontSize: "1.0rem",
-                        fontFamily: "Montserrat, san-serif",
-                        fontWeight: "bold",
-                        backgroundColor: (!isDisabled) ? "primary.main" : "grey.200",
-                        color: (!isDisabled) ? "grey.200" : "primary.main",
-                    }}
-                    disabled={isDisabled}
-                >
-                    Compare Plans
-                </Button>
-            </div>
-        </Box>
+        <>
+            <MapContainer
+                className="district-plan-map"
+                preferCanvas={true}
+                maxBoundsViscosity={1}
+                center={planGeoJSON.properties.CENTER}
+                maxBounds={planGeoJSON.properties.MAX_BOUNDS}
+                zoom={planGeoJSON.properties.CURRENT_ZOOM}
+                minZoom={planGeoJSON.properties.MIN_ZOOM}
+                maxZoom={planGeoJSON.properties.MAX_ZOOM}
+            >
+                <GeoJSON
+                    key={JSON.stringify(planGeoJSON.geoJSON)}
+                    data={planGeoJSON.geoJSON}
+                    onEachFeature={onEachFeature}
+                    zIndex={1}
+                />
+            </MapContainer>
+        </>
     );
 }
 
@@ -163,7 +223,7 @@ function DistrictPlanSummary({ planGraphData, planPopulationData, selectedDistri
     }
 
     return (
-        <div className="data-component-graph-container">
+        <div className="district-plan-summary">
             <DistrictPlanDistributionSelection
                 selectedDistrict={selectedDistrict}
                 selectedDist={selectedDist}
@@ -202,70 +262,10 @@ function DistrictPlanDistributionSelection({ selectedDistrict, selectedDist, set
                 </Select>
             </FormControl>
             <div className="averia-serif text-lg pt-2 pb-1 pl-1 font-bold">{`Selected District:`}</div>
-            <div className="averia-serif text-base pl-1">{`${selectedDistrict}`}</div>
-            <div className="averia-serif text-lg pt-8 pb-1 font-bold pl-1">{`Total Population:`}</div>
-            <div className="averia-serif text-base pl-1">{`${planPopulationData.toLocaleString()}`}</div>
+            <div className="averia-serif text-base pl-1 pb-4">{`${selectedDistrict}`}</div>
+            <div className="averia-serif text-lg pb-1 font-bold pl-1">{`Total Population:`}</div>
+            <div className="averia-serif text-base pl-1 pb-4">{`${planPopulationData.toLocaleString()}`}</div>
         </div>
-    );
-}
-
-function DistrictPlanPopup({ planGeoJSON, setShowPlan }) {
-    return (
-        <div className="district-plan-popup flex flex-end">
-            <IconButton className="district-plan-popup-exit" size="small" component="span" onClick={() => { setShowPlan(false) }}>
-                <CloseIcon />
-            </IconButton>
-            <DistrictPlanMap planGeoJSON={planGeoJSON} />
-        </div>
-    );
-}
-
-function DistrictPlanMap({ planGeoJSON }) {
-    if (planGeoJSON === null || planGeoJSON.geoJSON === null || planGeoJSON.properties === null)
-        return;
-
-    const onEachFeature = (feature, layer) => {
-        let originalColor = "#FFFFFF";
-
-        const darkerColor = Color(originalColor).darken(0.25).hex();
-
-        layer.setStyle({
-            fillColor: originalColor,
-            fillOpacity: feature.properties.FOPACITY || 0.75,
-            color: feature.properties.COLOR || "#000000",
-            weight: 1,
-        });
-
-        layer.on({
-            mouseover: (e) => {
-                e.target.setStyle({ fillColor: darkerColor, weight: 3 });
-            },
-            mouseout: (e) => {
-                e.target.setStyle({ fillColor: originalColor, weight: 1 });
-            }
-        });
-    };
-
-    return (
-        <>
-            <MapContainer
-                className="district-plan-map"
-                preferCanvas={true}
-                maxBoundsViscosity={1}
-                center={planGeoJSON.properties.CENTER}
-                maxBounds={planGeoJSON.properties.MAX_BOUNDS}
-                zoom={planGeoJSON.properties.CURRENT_ZOOM}
-                minZoom={planGeoJSON.properties.MIN_ZOOM}
-                maxZoom={planGeoJSON.properties.MAX_ZOOM}
-            >
-                <GeoJSON
-                    key={JSON.stringify(planGeoJSON.geoJSON)}
-                    data={planGeoJSON.geoJSON}
-                    onEachFeature={onEachFeature}
-                    zIndex={1}
-                />
-            </MapContainer>
-        </>
     );
 }
 
