629cb55cefa04e7946f763ec7993a3a1a4b0f05c
Kelvin Lin <kelvin.lin@stonybrook.edu>
2024-12-18 05:14:47 -0500

    Fixed buttons on the Gingles Tab

--- lobos-client/src/components/DataAnalysisComponents/GinglesTab.jsx
+++ lobos-client/src/components/DataAnalysisComponents/GinglesTab.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import IncomeVotingScatter from "../GraphPlotComponents/IncomeVotingScatter";
 import PrecinctDataTable from "../GraphPlotComponents/PrecinctDataTable";
 import VoteShareSeatShareGraph from "../GraphPlotComponents/VoteShareSeatShareGraph";
-import { Tooltip } from "@mui/material";
+import { Box, ButtonGroup, Button, Tooltip } from "@mui/material";
 import { States } from "../../enums";
 
 export default function GinglesTab({ selectedState }) {
@@ -50,93 +50,125 @@ export default function GinglesTab({ selectedState }) {
     );
 }
 
-export function GinglesButtons({ selectedState, selectedFilter, setSelectedFilter, selectedAdditionalView, setSelectedAdditionalView, isCurveDisabled }) {
+function GinglesButtons({ selectedState, selectedFilter, setSelectedFilter, selectedAdditionalView, setSelectedAdditionalView, isCurveDisabled }) {
     return (
-        <div className="flex items-center">
-            <div className="flex-grow">
-                <button
-                    className={
-                        selectedFilter === "income"
-                            ? "font-semibold border-2 border-black rounded-xl mr-4 p-1 pl-2 pr-2 bg-blue-400 shadow-2xl text-white"
-                            : "font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-2 pr-2 hover:bg-blue-200 shadow-2xl"
-                    }
-                    onClick={() => setSelectedFilter("income")}
-                >
-                    Income
-                </button>
-                <button
-                    className={
-                        selectedFilter === "race"
-                            ? "font-semibold border-2 border-black rounded-xl mr-4 p-1 pl-2 pr-2 bg-blue-400 shadow-2xl text-white"
-                            : "font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-2 pr-2 hover:bg-blue-200 shadow-2xl"
-                    }
-                    onClick={() => setSelectedFilter("race")}
-                >
-                    Racial/Ethnic
-                </button>
-                <button
-                    className={
-                        selectedFilter === "income&race"
-                            ? "font-semibold border-2 border-black rounded-xl mr-4 p-1 pl-2 pr-2 bg-blue-400 shadow-2xl text-white"
-                            : "font-semibold border-2 border-black mr-4 rounded-xl p-1 pl-2 pr-2 hover:bg-blue-200 shadow-2xl"
-                    }
-                    onClick={() => setSelectedFilter("income&race")}
-                >
-                    Income & Race
-                </button>
-            </div>
-            <div className="flex items-center space-x-4 ml-4">
-                <Tooltip
-                    title={
-                        isCurveDisabled
-                            ? `Disabled for ${selectedState}`
-                            : ""
-                    }
-                    placement="top"
-                    arrow
-                >
-                    <span>
-                        <button
-                            onClick={() =>
-                                setSelectedAdditionalView(
-                                    selectedAdditionalView === "curve"
-                                        ? null
-                                        : "curve"
-                                )
-                            }
-                            className={`text-sm font-semibold p-2 rounded-md ${selectedAdditionalView === "curve"
-                                ? "bg-blue-500 text-white"
-                                : "bg-gray-200 hover:bg-gray-300"
-                                }`}
-                            disabled={isCurveDisabled}
-                        >
-                            Seats-Votes Curve
-                        </button>
-                    </span>
-                </Tooltip>
-                <button
-                    onClick={() => {
-                        setSelectedAdditionalView(
-                            selectedAdditionalView === "table"
-                                ? null
-                                : "table"
-                        )
-                    }}
-                    className={`text-sm font-semibold p-2 rounded-md ${selectedAdditionalView === "table"
-                        ? "bg-blue-500 text-white"
-                        : "bg-gray-200 hover:bg-gray-300"
-                        }`}
-                >
-                    Display Table
-                </button>
-            </div>
+        <div className="flex items-center justify-between pb-2">
+            <GinglesTypeSelection selectedFilter={selectedFilter} setSelectedFilter={setSelectedFilter} />
+            <GinglesDisplayOptions selectedState={selectedState} selectedAdditionalView={selectedAdditionalView} setSelectedAdditionalView={setSelectedAdditionalView} isCurveDisabled={isCurveDisabled} />
         </div>
     );
 }
 
-export function GinglesChart({ selectedAdditionalView, selectedState, selectedFilter, setSelectedGEOID, setPrecinctData }) {
+function GinglesTypeSelection({ selectedFilter, setSelectedFilter }) {
+    const filterOptions = [
+        { text: "Income", value: "income" },
+        { text: "Racial/Ethnic", value: "race" },
+        { text: "Income & Race", value: "income&race" }
+    ]
+
+    return (
+        <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 2 }}>
+            <ButtonGroup
+                variant="contained"
+                aria-label="linked button group"
+                orientation="horizontal"
+            >
+                {filterOptions.map((element) => (
+                    <GinglesTypeButton
+                        key={element}
+                        selectedElement={selectedFilter}
+                        setSelectedElement={setSelectedFilter}
+                        buttonType={element}
+                    />
+                ))}
+            </ButtonGroup>
+        </Box>
+    );
+}
+
+function GinglesDisplayOptions({ selectedState, selectedAdditionalView, setSelectedAdditionalView, isCurveDisabled }) {
+    return (
+        <div className="flex items-center space-x-4 ml-4">
+            <Tooltip
+                title={
+                    isCurveDisabled
+                        ? `Disabled for ${selectedState}`
+                        : ""
+                }
+                placement="top"
+                arrow
+            >
+                <Button
+                    onClick={() => { setSelectedAdditionalView(selectedAdditionalView === "curve" ? null : "curve") }}
+                    sx={{
+                        textTransform: 'none',
+                        padding: "4px 8px",
+                        minHeight: "24px",
+                        fontSize: "1.0rem",
+                        fontFamily: "Montserrat, san-serif",
+                        textDecoration: (selectedAdditionalView === "curve") ? "underline" : "none",
+                        color: "#37474f",
+                        "&:hover": {
+                            backgroundColor: "grey.300",
+                        },
+                    }}
+                    disabled={isCurveDisabled}
+                    style={{
+                        cursor: isCurveDisabled ? "not-allowed" : "pointer",
+                    }}
+                >
+                    Seats-Votes Curve
+                </Button>
+            </Tooltip>
+            <Button
+                onClick={() => { setSelectedAdditionalView(selectedAdditionalView === "table" ? null : "table") }}
+                sx={{
+                    textTransform: 'none',
+                    padding: "4px 8px",
+                    minHeight: "24px",
+                    fontSize: "1.0rem",
+                    fontFamily: "Montserrat, san-serif",
+                    textDecoration: (selectedAdditionalView === "table") ? "underline" : "none",
+                    color: "#37474f",
+                    "&:hover": {
+                        backgroundColor: "grey.300",
+                    },
+                }}
+            >
+                Display Table
+            </Button>
+        </div>
+    );
+}
+
+function GinglesTypeButton({ selectedElement, setSelectedElement, buttonType }) {
+    const isButtonSelected = (selectedElement === buttonType.value);
+
+    return (
+        <Button
+            onClick={() => setSelectedElement(buttonType.value)}
+            sx={{
+                textTransform: 'none',
+                padding: "4px 12px",
+                minHeight: "32px",
+                fontSize: "1.0rem",
+                fontFamily: "Montserrat, san-serif",
+                backgroundColor: isButtonSelected ? "primary.main" : "grey.200",
+                color: isButtonSelected ? "grey.200" : "primary.main",
+                transition: "all 0.3s ease-in-out",
+                "&:hover": {
+                    backgroundColor: isButtonSelected ? "primary.dark" : "grey.300",
+                },
+            }}
+        >
+            {buttonType.text}
+        </Button>
+    );
+}
+
+function GinglesChart({ selectedAdditionalView, selectedState, selectedFilter, setSelectedGEOID, setPrecinctData }) {
     const additionalCSS = `${(selectedAdditionalView) ? "h-2/4" : "h-3/4"}`;
-    
+
     return (
         <div className={`transition-all duration-300 w-full mt-4 pb-3 ${additionalCSS}`}>
             <IncomeVotingScatter
@@ -149,7 +181,7 @@ export function GinglesChart({ selectedAdditionalView, selectedState, selectedFi
     );
 }
 
-export function GinglesTable({ precinctData, selectedGEOID }) {
+function GinglesTable({ precinctData, selectedGEOID }) {
     return (
         <div className="flex-grow w-full overflow-hidden mt-6 pt-5">
             <PrecinctDataTable
@@ -160,7 +192,7 @@ export function GinglesTable({ precinctData, selectedGEOID }) {
     );
 }
 
-export function GinglesCurve({ selectedState, isCurveDisabled }) {
+function GinglesCurve({ selectedState, isCurveDisabled }) {
     if (isCurveDisabled)
         return;
 
